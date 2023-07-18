de5cc79e0d528099a2ea9cad07ec2ae4443be137
Janek Ebbers <ebbers@nt.upb.de>
2023-07-18 16:33:10 +0200

    update resample_db to be applicable to all databases

--- pb_sed/database/resample_db.py
+++ pb_sed/database/resample_db.py
@@ -2,47 +2,105 @@
 import shutil
 import click
 import paderbox as pb
+import numpy as np
 from pathlib import Path
 from concurrent.futures import ProcessPoolExecutor
 from tqdm import tqdm
 import soundfile as sf
 
 
-def process_example(source_path, destination_path, relative_audio_path, out_rate):
-    source_audio_path = source_path/relative_audio_path
-    if not source_audio_path.suffix == ".wav" or not source_audio_path.exists():
-        print(f"skipping {source_audio_path} because it is not a wav file")
-        return None
-    destination_audio_path = destination_path/relative_audio_path
-    with sf.SoundFile(str(source_audio_path)) as f:
-        detected_in_rate = f.samplerate
-
-    if out_rate == detected_in_rate:
-        if not destination_audio_path.parent.exists():
-            destination_audio_path.parent.mkdir(parents=True, exist_ok=True)
-        shutil.copy(str(source_audio_path), str(destination_audio_path))
+def process_file(
+        source_path, destination_path, relative_path,
+        audio_output_rate, audio_output_format,
+        skip_existing=False, excluded=(),
+):
+    source_file_path = source_path / relative_path
+    if source_file_path.is_dir():
+        return False
+    if any([relative_path.startswith(excl) for excl in excluded]):
+        return False
+    source_file_split = source_file_path.name.rsplit(
+        '.', maxsplit=1)
+    if len(source_file_split) == 1:
+        source_file_stem, = source_file_split
+        source_file_suffix = ''
     else:
-        audio_data = pb.io.load(source_audio_path)
-        resampled_audio_data = pb.transform.resample_sox(
-            audio_data, in_rate=detected_in_rate, out_rate=out_rate
+        source_file_stem, source_file_suffix = source_file_split
+    if not source_file_stem:
+        return False
+    destination_file_path = destination_path / relative_path
+    if not destination_file_path.parent.exists():
+        destination_file_path.parent.mkdir(parents=True, exist_ok=True)
+    if source_file_suffix == 'wav':
+        destination_file_path = (
+            destination_file_path.parent
+            / f"{source_file_stem}.{audio_output_format}"
         )
-        if not destination_audio_path.exists():
-            pb.io.dump(resampled_audio_data, destination_audio_path, mkdir=True, mkdir_exist_ok=True, mkdir_parents=True)
+        if destination_file_path.exists() and skip_existing:
+            return True
+        try:
+            with sf.SoundFile(str(source_file_path)) as f:
+                detected_in_rate = f.samplerate
+        except sf.LibsndfileError:
+            return False
+        if audio_output_rate == detected_in_rate and audio_output_format == source_file_suffix:
+            shutil.copy(str(source_file_path), str(destination_file_path))
+        else:
+            audio_data = pb.io.load(source_file_path)
+            if (audio_data.size == 0) or (np.max(np.abs(audio_data)) < 1e-12):
+                return False
+            if audio_output_rate != detected_in_rate:
+                audio_data = pb.transform.resample_sox(
+                    audio_data, in_rate=detected_in_rate, out_rate=audio_output_rate
+                )
+            pb.io.dump_audio(audio_data, destination_file_path, sample_rate=audio_output_rate, format=audio_output_format)
+    elif not destination_file_path.exists() or not skip_existing:
+        shutil.copy(str(source_file_path), str(destination_file_path))
+    return True
 
 
-def print_example(source_path, destination_path, relative_audio_path):
-    source_audio_path = source_path/relative_audio_path
-    destination_audio_path = destination_path/relative_audio_path
-    print(f"write resampled {source_audio_path} to {destination_audio_path}")
-    
+def print_example(
+        source_path, destination_path, relative_path,
+        audio_output_rate, audio_output_format,
+        skip_existing=False, excluded=(),
+):
+    source_file_path = source_path / relative_path
+    if source_file_path.is_dir():
+        return False
+    if any([relative_path.startswith(excl) for excl in excluded]):
+        return False
+    source_file_split = source_file_path.name.rsplit(
+        '.', maxsplit=1)
+    if len(source_file_split) == 1:
+        source_file_stem, = source_file_split
+        source_file_suffix = ''
+    else:
+        source_file_stem, source_file_suffix = source_file_split
+    destination_file_path = destination_path / relative_path
+    if source_file_suffix in ['wav', 'flac']:
+        destination_file_path = (
+            destination_file_path.parent
+            / f"{source_file_stem}.{audio_output_format}"
+        )
+        if destination_file_path.exists() and skip_existing:
+            print(f"{destination_file_path} already exists")
+        else:
+            print(f"write (resampled) {source_file_path} to {destination_file_path}")
+    elif destination_file_path.exists() and skip_existing:
+        print(f"{destination_file_path} already exists")
+    else:
+        print(f"copy {source_file_path} to {destination_file_path}")
+    return True
+
 
 @click.command()
-@click.option("--source_path", help="Path to original database directory")
-@click.option("--destination_path", help="Path of the new database root directory")
-@click.option("--out_rate", default=16000, help="Desired sample rate")
-@click.option("--num_workers", default=50, help="Number of resampling threads")
+@click.option("--source_path", "-s", help="Path to original database directory")
+@click.option("--destination_path", "-d", help="Path of the new database root directory")
+@click.option("--out_rate", "-r", default=16000, help="Desired sample rate")
+@click.option("--out_format", "-f", default='flac', help="Desired output format")
+@click.option("--excluded", "-e", default=None, help="Excluded sub paths separated by comma.")
+@click.option("--num_workers", "-w", default=50, help="Number of resampling threads")
 @click.option(
-    "-d",
     "--dry",
     is_flag=True,
     show_default=True,
@@ -50,7 +108,8 @@ def print_example(source_path, destination_path, relative_audio_path):
     help="Dry run, shows which files are resampled and where they are saved",
 )
 @click.option(
-    "--batching",
+    "-b",
+    "--batch_size",
     default=100,
     help="If positive integer, size of the batches, otherwise no batching if None",
 )
@@ -58,88 +117,67 @@ def print_example(source_path, destination_path, relative_audio_path):
               is_flag=True,
               default=False,
               help="Continue if destination directory exists")
-@click.option("--audio_file_glob",
-              default='audio/**/*.wav',
-              help="Glob used to find audio files inside the database directory")
-@click.option("--continue", 'continue_', is_flag=True, default=False,
-             help="Continue resampling, skipping all existing files")
+@click.option("--skip_existing", 'skip_existing', is_flag=True, default=True,
+              help="Continue resampling, skipping all existing files")
 def resample_db(
     source_path,
     destination_path,
     out_rate,
+    out_format,
+    excluded,
     num_workers,
     dry,
-    batching,
+    batch_size,
     exist_ok,
-    audio_file_glob,
-    continue_,
+    skip_existing,
 ):
-    source_path = Path(source_path)
+    source_path = Path(source_path).expanduser().absolute()
+    destination_path = Path(destination_path).expanduser().absolute()
+
     assert source_path.exists(), f"Source database not found at {source_path}"
-    if not exist_ok and not continue_:
-        assert not Path(destination_path).exists(), f"Destination already exists: {destination_path}"
-
-    if source_path.is_dir():
-        source_db_path = source_path
-        audio_path_list = source_db_path.rglob(audio_file_glob)
-    else:
-        raise ValueError(f"The given source directory {source_path} is not a directory.")
-
-    # ignore existing files, this does not check for correctess of the file content
-    if continue_:
-        existing_files = set([p.relative_to(destination_path) for p in Path(destination_path).rglob(audio_file_glob)])
-
-    relative_audio_paths = []
-    for audio_path in audio_path_list:
-        relative_path = audio_path.relative_to(source_path)
-        if not continue_ or relative_path not in existing_files:
-           relative_audio_paths.append(relative_path)
-
-    if batching is not None:
-        try:
-            batching = int(batching)
-            if batching > 1:
-                batch_size = batching
-        except ValueError:
-            pass
-    else:
-        batch_size = 1
+    assert source_path.is_dir(), f"The given source directory {source_path} is not a directory."
+    assert exist_ok or not destination_path.exists(), f"Destination already exists: {destination_path}"
 
     if dry:
         print("dry run")
     else:
-        Path(destination_path).mkdir(exist_ok=True, parents=True)
+        destination_path.mkdir(exist_ok=exist_ok, parents=True)
+    file_list = list(source_path.rglob('*'))
+
+    print(f'Found {len(file_list)} files in source directory')
+
+    excluded = [] if excluded is None else excluded.split(',')
+    relative_file_paths = [str(file_path.relative_to(source_path)) for file_path in file_list]
+
+    if batch_size is None:
+        batch_size = 1
+    else:
+        batch_size = int(batch_size)
 
     # load/resample/write files
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        process_fn = process_example
+        process_fn = process_file
         if dry:
-            relative_audio_paths = relative_audio_paths[:10]
+            relative_file_paths = relative_file_paths[:100]
             process_fn = print_example
-        num_tasks = len(relative_audio_paths)
-        list(
+        num_tasks = len(relative_file_paths)
+        n_files = sum(list(
             tqdm(
                 executor.map(
                     process_fn,
                     num_tasks * [source_path],
                     num_tasks * [destination_path],
-                    relative_audio_paths,
+                    relative_file_paths,
                     num_tasks * [out_rate],
+                    num_tasks * [out_format],
+                    num_tasks * [skip_existing],
+                    num_tasks * [excluded],
                     chunksize=batch_size,
                 ),
                 total=num_tasks,
             )
-        )
-    # copy annotation files
-    annotation_files = source_db_path.glob("*")
-    if dry:
-        annotation_files = []
-    for annotation_file in annotation_files:
-        if annotation_file.is_file():
-            file_name = annotation_file.name
-            shutil.copy(str(annotation_file), str(destination_path/file_name))
-
-    print(f'Resampled database created at {destination_path}')
+        ))
+    print(f'Resampled database created at {destination_path} with {n_files} files')
 
 
 if __name__ == "__main__":
